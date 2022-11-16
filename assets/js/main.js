7c98845f921cfdcb732a37ef5a5b4478cd69444e
Sky Luo <skluo@davidson.edu>
2022-11-16 10:50:25 -0500

    Javascript changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,26 @@
 /* javascript */
-var record = []
-var computerGuess = 0
-var playerScore = 0
-var computerScore = 0
-var computerHit = 0
-
-document.getElementById("button-1").addEventListener("click", main())
-document.getElementById("button-2").addEventListener("click",  main())
-
-function main(){
-  recordHit()
-  computerGuess()
-  if(!verifyComputerGuess()){
-    playerScore++
-    return
-  } else{ //computer makes a right guess
-    computerMakeAHit()
-  }
+var record = [];
+var computerGuess = 0;
+var playerScore = 0;
+var computerScore = 0;
+var computerHit = 0;
 
 
+document.getElementById("button-1").addEventListener("click", recordHit(1), mainFunction());
+document.getElementById("button-2").addEventListener("click",  recordHit(2), mainFunction());
+
+function recordHit(buttonNumber){
+  record.push(buttonNumber)
 }
 
-function recordHit(){
-  record.push('1')
-}
-
-function computerGuess(){
+function computerMakeGuess(){
   if (record.length < 2) {
     computerGuess = Math.floor(Math.random()*2+1)
     return
   }
-  if (record[-3]===1 && record[-2]===2) { //the most recent two-letter sequence is 1 2
+
+  //the most recent two-letter sequence is 1 2
+  if (record[-3]===1 && record[-2]===2) {
     var followedByOne = 0
     var followedByTwo = 0
     for (let i=0; i < record.length-1; i++){
@@ -49,7 +39,10 @@ function computerGuess(){
     } else if (followedByOne === followedByTwo) {
       computerGuess = Math.floor(Math.random()*2+1)
     }
-  } else if (record[-3]===2 && record[-2]===1){ //the most recent two-letter sequence is 2 1
+  }
+
+  //the most recent two-letter sequence is 2 1
+  if (record[-3]===2 && record[-2]===1){
     var followedByOne = 0
     var followedByTwo = 0
     for (let i=0; i < record.length-1; i++){
@@ -68,7 +61,9 @@ function computerGuess(){
     } else if (followedByOne === followedByTwo) {
       computerGuess = Math.floor(Math.random()*2+1)
     }
-  } else if (record[-3]===1 && record[-2]===1){ //the most recent two-letter sequence is 1 1
+  }
+  //the most recent two-letter sequence is 1 1
+  if (record[-3]===1 && record[-2]===1){
     var followedByOne = 0
     var followedByTwo = 0
     for (let i=0; i < record.length-1; i++){
@@ -87,7 +82,10 @@ function computerGuess(){
     } else if (followedByOne === followedByTwo) {
       computerGuess = Math.floor(Math.random()*2+1)
     }
-  } else if (record[-3]===2 && record[-2]===2){ //the most recent two-letter sequence is 2 2
+  }
+
+  //the most recent two-letter sequence is 2 2
+  if (record[-3]===2 && record[-2]===2){
     var followedByOne = 0
     var followedByTwo = 0
     for (let i=0; i < record.length-1; i++){
@@ -118,5 +116,31 @@ function verifyComputerGuess(){
 }
 
 function computerMakeAHit(){
-  computerHit = Math.floor(Math.random()*2+1)
+  computerHit = Math.floor(Math.random()*2+5)
+}
+
+function mainFunction(buttonNumber){
+  recordHit(buttonNumber);
+  console.log(record);
+  computerMakeGuess();
+  if(record.length != 0 && !verifyComputerGuess()){
+    alert("You won this point!")
+    playerScore++;
+    document.getElementById("your-score").textContent=playerScore;
+    return;
+  } else { //computer makes a right guess, points goes on
+    computerMakeAHit();
+    alert("The computer returned your shot! You need to make a return decision!");
+  }
+  // I need to have the user make an input to continue this funtion
+  var userGuess = 0
+  document.getElementById("button-5").addEventListener("click", userGuess=5);
+  document.getElementById("button-6").addEventListener("click", userGuess=6);
+   // after the user makes a choice
+  if (userguess===computerHit) {
+    alert("You returned computer's hit! Now make another hitting decision!")
+  } else{
+    computerScore++;
+    document.getElementById("computer-score").textContent= computerScore;
+  }
 }
