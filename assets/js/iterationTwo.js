245e46bce113bc31afe204442f3f630e105b8937
Sky Luo <skluo@davidson.edu>
2022-11-24 21:51:06 -0500

    buttons to color changing areas

--- assets/js/iterationTwo.js
+++ assets/js/iterationTwo.js
@@ -10,16 +10,29 @@ setTimeout(function(){
   document.getElementById("title-box").style.backgroundColor="Red"
   document.getElementById("title").textContent="Hit!"
   document.getElementById("title").style.fontSize="25px"
-  showHitHideReturn()
 }, 2000)
 
 document.getElementById("button-1").addEventListener("click", function(){
   mainFunction(1);
   console.log("Your hitting record is: ", record, ". This is computer's guess",  computerGuess)
 });
+$("#button-1").hover(function(){
+  $("#button-1").css("background-color", "yellow");
+  $("#button-1-hit-here").css("color","red");
+  }, function(){
+  $("#button-1").css("background-color", "#51778D");
+  $("#button-1-hit-here").css("color","#51778D");
+});
 document.getElementById("button-2").addEventListener("click", function(){
   mainFunction(2);
   console.log("Your hitting record is: ", record, ". This is computer's guess: ",  computerGuess)
+});
+$("#button-2").hover(function(){
+  $("#button-2").css("background-color", "yellow");
+  $("#button-2-hit-here").css("color","red");
+  }, function(){
+  $("#button-2").css("background-color", "#51778D");
+  $("#button-2-hit-here").css("color","#51778D");
 });
 
 function recordHit(buttonNumber){
@@ -136,20 +149,6 @@ function computerMakeAHit(){
   computerHit = Math.floor(Math.random()*2+5)
 }
 
-function showHitHideReturn(){
-  document.getElementById("button-1").style.display="inline"
-  document.getElementById("button-2").style.display="inline"
-  document.getElementById("button-5").style.display="none"
-  document.getElementById("button-6").style.display="none"
-}
-
-function showReturnHideHit(){
-  document.getElementById("button-1").style.display="none"
-  document.getElementById("button-2").style.display="none"
-  document.getElementById("button-5").style.display="inline"
-  document.getElementById("button-6").style.display="inline"
-}
-
 function youLost(){
   document.getElementById("button-1").style.display="none"
   document.getElementById("button-2").style.display="none"
@@ -179,13 +178,11 @@ function mainFunction(buttonNumber){
     if (playerScore===5) {
       youWon()
     }
-    showHitHideReturn()
     return;
   }
   //computer makes a right guess, points goes on
   else {
     computerMakeAHit()
-    showReturnHideHit()
     document.getElementById("title").textContent="The computer returned! You need to return!"
   }
 }
@@ -200,14 +197,12 @@ document.getElementById("button-5").addEventListener("click", function(){
     if(computerScore===5){
       youLost()
     }
-    showHitHideReturn()
     setTimeout(function(){
       document.getElementById("title").textContent="Next point. Hit!"
     }, 1500)
   }
   // if user makes a correct guess
   else{
-    showHitHideReturn()
     document.getElementById("title").textContent="You sucessfully returned! Now hit!"
   }
 })
@@ -222,7 +217,6 @@ document.getElementById("button-6").addEventListener("click", function(){
     if(computerScore===5){
       youLost()
     }
-    showHitHideReturn()
     setTimeout(function(){
       document.getElementById("title").textContent="Next point. Hit!"
     }, 1500)
@@ -230,6 +224,5 @@ document.getElementById("button-6").addEventListener("click", function(){
   // if user makes a correct guess
   else{
     document.getElementById("title").textContent="You guessed computer's hit! Now hit!"
-    showHitHideReturn()
   }
 })
